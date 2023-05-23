e6d9171c520cd5b2bdebf56e02ef4c21cbdf278a
sean <sean@campbelltech.co.za>
2023-05-23 15:50:45 +0200

    Added missing files to source

---
+++ src/main/java/com/campbelltech/simplefactoryspring/SimpleFactorySpringApplication.java
@@ -0,0 +1,13 @@
+package com.campbelltech.simplefactoryspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFactorySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFactorySpringApplication.class, args);
+	}
+
+}
